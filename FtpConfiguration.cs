992f20cd393d018159f1f3a5691cedac2b2d52d4
Marco Ruch <marco_ruch@gmx.ch>
2021-04-30 19:10:26 +0200

    Create FtpConfiguration.cs

---
+++ FtpConfiguration.cs
@@ -0,0 +1,11 @@
+namespace Shared.Utilities
+{
+    public class FtpConfiguration
+    {
+        public bool IsSFTP { get; set; }
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
